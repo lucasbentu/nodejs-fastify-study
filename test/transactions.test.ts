08f31a4f24b383d91de3059330f53502872860c7
lucasbentu <lucas.bento38@gmail.com>
2023-06-02 08:57:08 -0300

    feat(test):finish all tests app

--- test/transactions.test.ts
+++ test/transactions.test.ts
@@ -52,7 +52,6 @@ describe('Transactions routes', () => {
       }),
     ])
   })
-
   it('should be able to get a specific transaction', async () => {
     const transactionCreatedResponse = await request(app.server)
       .post('/transactions')
@@ -82,5 +81,34 @@ describe('Transactions routes', () => {
         amount: 5000,
       }),
     )
+  })
+  it('should be able to get the summary', async () => {
+    const transactionCreatedResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Credit transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = transactionCreatedResponse.get('Set-Cookie')
+
+    await request(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies)
+      .send({
+        title: 'Debit transaction',
+        amount: 2000,
+        type: 'debit',
+      })
+
+    const summaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(summaryResponse.body.summary).toEqual({
+      amount: 3000,
+    })
   })
 })
